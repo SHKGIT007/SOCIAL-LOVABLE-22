0fc315dd0e47b3bb25c62375f3825047e254993f
SHKGIT007 <shkgit007@gmail.com>
2025-10-14 17:55:16 +0530

    Done

---
+++ backend/app/jobs/runScheduler.js
@@ -0,0 +1,14 @@
+// runScheduler.js
+// This script runs the scheduled post publisher job every minute using node-cron
+
+const cron = require('node-cron');
+const { publishScheduledPosts } = require('./scheduledPostPublisher');
+
+// Schedule the job to run every minute
+cron.schedule('* * * * *', async () => {
+  await publishScheduledPosts();
+  console.log('Scheduled posts checked and published if due.');
+});
+
+// Keep process alive
+console.log('Scheduled post publisher started.');
